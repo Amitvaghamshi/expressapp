d0ac9ac2a2a4ced6ca5b499b349e6700b18e9010
amit <amitvaghamshi9@gmail.com>
2026-04-25 15:17:34 +0530

    code changed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("hello world by amit. ");
+    res.send("hello world by amit bhai");
 })
 
 app.get("/hello", (req, res) => {
