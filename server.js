59fb24f672f8d429b2e00b995c1dc03091825125
amit <amitvaghamshi9@gmail.com>
2026-04-25 15:05:31 +0530

    Amit | code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("hello world by amit");
+    res.send("hello world by amit. ");
 })
 
 app.get("/hello", (req, res) => {
